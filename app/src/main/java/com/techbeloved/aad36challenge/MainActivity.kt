232be9454b22871179fc8d36b670a7566c2a9202
odifek <kennedy998@gmail.com>
2019-10-20 00:06:12 +0100

    WIP: implement game play logic #8
    Scene transition is not working properly yet. When transitioning to play scene after the result scene, nothing is displayed on the screen!

--- app/src/main/java/com/techbeloved/aad36challenge/MainActivity.kt
+++ app/src/main/java/com/techbeloved/aad36challenge/MainActivity.kt
@@ -2,11 +2,18 @@ package com.techbeloved.aad36challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Scene
-import android.transition.TransitionManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.transition.Scene
+import androidx.transition.TransitionManager
+import com.google.android.material.button.MaterialButton
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.scene_correct.*
+import kotlinx.android.synthetic.main.scene_correct.button_continue
+import kotlinx.android.synthetic.main.scene_default.*
+import kotlinx.android.synthetic.main.scene_incorrect.*
+import kotlinx.android.synthetic.main.scene_play.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,20 +22,87 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
         viewModel = ViewModelProviders.of(this)[TriviaViewModel::class.java]
 
         viewModel.triviaState.observe(this, Observer { triviaState ->
+            Timber.i("New State: $triviaState")
             when (triviaState) {
                 TriviaState.Default -> {
                     val defaultScene =
                         Scene.getSceneForLayout(sceneRoot, R.layout.scene_default, this)
                     TransitionManager.go(defaultScene)
+
+                    if (!button_play.hasOnClickListeners()) {
+                        button_play.setOnClickListener {
+                            if (editText_player_name.text.isNullOrBlank()) {
+                                editText_player_name.error = "Please enter name"
+                            } else {
+                                val playerName = editText_player_name.text.toString()
+                                viewModel.startNewGame(playerName)
+                            }
+                        }
+                    }
                 }
-                is TriviaState.Play -> TODO()
-                is TriviaState.Correct -> TODO()
-                is TriviaState.Incorrect -> TODO()
-                is TriviaState.Timeout -> TODO()
-                is TriviaState.Summary -> TODO()
+                is TriviaState.Play -> {
+                    val question = triviaState.question
+                    val playScene = Scene.getSceneForLayout(sceneRoot, R.layout.scene_play, this)
+                    // Transition to scene
+                    TransitionManager.go(playScene)
+                    textView_play_question.text = question.question
+                    textView_play_question_type.text = question.category
+                    textView_play_counter.text =
+                        getString(R.string.question_counter, triviaState.index, triviaState.total)
+
+                    // Remove previous views that might still be visible in the linear layout
+                    linearLayout_play_options.removeAllViews()
+                    question.answers.forEach { answer ->
+                        val button = layoutInflater.inflate(
+                            R.layout.widget_button_choice,
+                            linearLayout_play_options,
+                            false
+                        ) as MaterialButton
+                        button.text = answer
+                        button.setTag(R.id.answer_choice, answer)
+                        button.setOnClickListener { viewModel.selectedAnswer(answer) }
+
+                        linearLayout_play_options.addView(button)
+                    }
+                }
+                is TriviaState.GameOn.Correct -> {
+                    val correctScene =
+                        Scene.getSceneForLayout(sceneRoot, R.layout.scene_correct, this)
+                    TransitionManager.go(correctScene)
+                    if (!triviaState.hasMore) {
+                        button_continue.setText(R.string.see_summary)
+                    }
+                    if (!button_continue.hasOnClickListeners()) {
+                        button_continue.setOnClickListener {
+                            viewModel.next()
+                        }
+                    }
+                }
+                is TriviaState.GameOn.Incorrect -> {
+                    val incorrectScene =
+                        Scene.getSceneForLayout(sceneRoot, R.layout.scene_incorrect, this)
+                    TransitionManager.go(incorrectScene)
+                    if (!triviaState.hasMore) {
+                        button_continue.setText(R.string.see_summary)
+                    }
+                    if (!button_continue.hasOnClickListeners()) {
+                        button_continue.setOnClickListener {
+                            viewModel.next()
+                        }
+                    }
+                }
+                is TriviaState.GameOn.Timeout -> {
+                    // TODO
+                }
+                is TriviaState.Summary -> {
+                    // TODO
+                }
             }
         })
     }
--- app/src/main/java/com/techbeloved/aad36challenge/TriviaViewModel.kt
+++ app/src/main/java/com/techbeloved/aad36challenge/TriviaViewModel.kt
@@ -3,6 +3,12 @@ package com.techbeloved.aad36challenge
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.google.gson.JsonParseException
+import com.techbeloved.aad36challenge.api.OpenTriviaResults
+import com.techbeloved.aad36challenge.api.SAMPLE_RESPONSE
+import timber.log.Timber
+import java.util.*
 
 /**
  * All game decisions, retrieving questions, creating a new game and a new player will be done in this viewModel
@@ -11,12 +17,100 @@ import androidx.lifecycle.ViewModel
  *
  * Also, all user inputs are forwarded here for decision making
  */
-class TriviaViewModel: ViewModel() {
+class TriviaViewModel : ViewModel() {
 
+    private var _currentQuestion: Question? = null
     private val _triviaState: MutableLiveData<TriviaState> = MutableLiveData(TriviaState.Default)
+
+    private var _currentPlayer: Player? = null
+    private var _nextQuestionIndex = 0
 
     /**
      * Observe this to receive the current state of the game
      */
     val triviaState: LiveData<TriviaState> = _triviaState
+
+    fun startNewGame(playerName: String) {
+
+        val gson = Gson()
+        try {
+            val openTriviaResults: OpenTriviaResults =
+                gson.fromJson(SAMPLE_RESPONSE, OpenTriviaResults::class.java)
+            if (openTriviaResults.responseCode == 0) {
+                val newGame = Game(
+                    id = UUID.randomUUID().toString(),
+                    completed = false,
+                    choices = mutableMapOf(),
+                    questions = openTriviaResults.results.map { trivia -> trivia.toQuestion() }
+                )
+                _currentPlayer = Player(
+                    name = playerName,
+                    game = newGame,
+                    score = 0
+                )
+                val nextQuestion = selectNextQuestion()
+                if (nextQuestion != null) {
+                    dispatchNextQuestion(nextQuestion)
+                }
+            } else {
+                Timber.w("Error getting getting questions")
+
+            }
+        } catch (e: JsonParseException) {
+            Timber.w(e, "Error parsing response!")
+        }
+
+    }
+
+    /**
+     * Called with the answer selected by the player
+     * @param answer The choice made by the player
+     */
+    fun selectedAnswer(answer: String) {
+        _currentQuestion?.let { question ->
+            _currentPlayer?.let { player ->
+                val hasMore = selectNextQuestion() != null
+                val nextState = if (question.correctAnswer == answer) {
+                    player.score.plus(question.difficulty.points)
+                    TriviaState.GameOn.Correct(question, answer, hasMore)
+                } else {
+                    TriviaState.GameOn.Incorrect(question, answer, hasMore)
+                }
+                player.game.choices[question.id] = answer
+                player.game.completed = !hasMore
+                _triviaState.value = nextState
+            }
+        }
+    }
+
+    private fun selectNextQuestion(): Question? {
+        return _currentPlayer?.let { player ->
+            if (_nextQuestionIndex < player.game.questions.size) player.game.questions[_nextQuestionIndex] else {
+                Timber.i("No more questions to play")
+                null
+            }
+        }
+    }
+
+    private fun dispatchNextQuestion(question: Question) {
+        _currentQuestion = question
+        val questionCount = _currentPlayer?.game?.questions?.count() ?: 0
+        _triviaState.value = TriviaState.Play(question, _nextQuestionIndex, questionCount)
+        _nextQuestionIndex++
+    }
+
+    fun next() {
+        when (val state = _triviaState.value) {
+            is TriviaState.GameOn-> {
+                val nextQuestion = selectNextQuestion()
+                if (state.hasMore && nextQuestion != null) {
+                    dispatchNextQuestion(nextQuestion)
+                } else {
+                    _currentPlayer?.let { player ->
+                        _triviaState.value = TriviaState.Summary(player)
+                    }
+                }
+            }
+        }
+    }
 }
