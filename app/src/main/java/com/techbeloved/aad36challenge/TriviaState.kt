dbcd5641054d59cb76b82bd877901a8a6927ec10
odifek <kennedy998@gmail.com>
2019-10-19 16:17:59 +0100

    add time out state and hasMore condition #2

--- app/src/main/java/com/techbeloved/aad36challenge/TriviaState.kt
+++ app/src/main/java/com/techbeloved/aad36challenge/TriviaState.kt
@@ -5,12 +5,14 @@ sealed class TriviaState {
 
     data class Play(val player: Player): TriviaState()
 
-    data class Correct(val question: Question, val choice: String): TriviaState()
+    data class Correct(val question: Question, val choice: String, val hasMore: Boolean): TriviaState()
 
-    data class Incorrect(val question: Question, val choice: String): TriviaState()
+    data class Incorrect(val question: Question, val choice: String, val hasMore: Boolean): TriviaState()
+
+    data class Timeout(val question: Question, val hasMore: Boolean): TriviaState()
 
     /**
-     * Should display summary of the game and
+     * Contains the summary of the game and how many points won by the player
      */
     data class Summary(val player: Player): TriviaState()
 }
